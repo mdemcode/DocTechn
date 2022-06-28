2c9129b5e123853c507451b1573bd000f211eb58
MDemCode <mdemcode@gmail.com>
2022-06-28 11:11:43 +0200

    Add project files.

---
+++ DokumentTechnologiczny.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DocTechn
+{
+    public abstract class DokumentTechnologiczny {
+
+        public string KodKreskowyTxt  { get; }
+        public bool DanePodstWczytanePoprawnie { get; }
+        public bool UtworzonaPoprawnie { get; protected set; }
+        //
+        public readonly List<string> Bledy = new();
+
+
+        protected DokumentTechnologiczny(string tekstKoduKresk) {
+            KodKreskowyTxt = tekstKoduKresk;
+            //DanePodstWczytanePoprawnie = OdczytajDaneKoduKresk(tekstKoduKresk);
+        }
+
+        protected abstract bool OdczytajDaneWgKoduKresk(string kodKresk);
+    }
+}
---
+++ KartyTechnologiczne/KartaTechnologiczna.cs
@@ -0,0 +1,46 @@
+﻿using PrzewodnikPLM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DocTechn.KartyTechnologiczne
+{
+    public abstract class KartaTechnologiczna : DokumentTechnologiczny {
+
+        protected readonly ZleceniePLM _zlecenie;
+        public int IdZlecDB => _zlecenie.IdDB;
+        public string NrZlec => _zlecenie.Nr;
+        public int KodZlec => _zlecenie.Kod;
+        public string Sekcja => _zlecenie.Sekcja;
+        //
+        public abstract string NrGr { get; }
+        public abstract string Lp { get; }
+        public abstract bool Hold { get; }
+        public abstract bool Uwolniony { get; }
+        public abstract StatusWykonania Status { get; }
+        /// <summary> Ilość sztuk detalu (przewodnik) lub elementu (karta montaż) </summary>
+        public abstract int Szt { get; }
+        public abstract List<OperacjaAsprova> Operacje { get; }
+        public abstract string Uwagi { get; }
+        public abstract string ToolTipText { get; }
+
+        public KartaTechnologiczna(string tekstKoduKresk) : base(tekstKoduKresk) { }
+
+    }
+}
+        ////
+
+        //protected KartaTechnologiczna(ZleceniePLM zlecPLM) {
+        //    if (zlecPLM is null || !zlecPLM.ZlecenieWczytanePopr) Bledy.Add("Błąd wczytywania zlecenia!");
+        //    _zlecenie = zlecPLM;
+        //}
+
+
+    public enum StatusWykonania {           // kombinacje dla montażu:
+        Nieznany,
+        DoWydania,                          // 'V' / 'V'
+        Wydany, // w trakcie wykonywania    // bryg / 'V'
+        Zlozony, // tylko montaż            // 'W' / bryg
+        Pospawany, // tylko montaż          // 'W' / 'W'
+        Wykonany                            // po frezarce ??
+    }
